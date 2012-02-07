1db5e266c826e2530b570608c87191f6b38448d1
David Guo <david.guo@uwaterloo.ca>
2012-02-07 15:03:32 -0800

    Initial asynchronous log implementation + updated tests

--- extension/lib/logger.js
+++ extension/lib/logger.js
@@ -1,65 +1,75 @@
-var { Cc, Ci } = require("chrome");
+const { Cc, Ci, Cu } = require("chrome"),
+      { Trait } = require("traits");
+var NetUtil = {};
+Cu.import("resource://gre/modules/NetUtil.jsm", NetUtil);
+NetUtil = NetUtil.NetUtil;
 
-
-function Logger(aDir) {
-  this._dir = aDir;
-
-  this.active = false;
-  this.file = null;
-}
-
-Logger.prototype = {
-
-
+var Logger = Trait.compose({
+  constructor: function Logger(options) {
+    this._dir = options.aDir;
+    this._foStream = null;
+    this._file = null;
+    this._active = false;
+    
+    // Converter to create input streams out of strings
+    this._converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"]
+                      .createInstance(Components.interfaces.nsIScriptableUnicodeConverter); 
+    this._converter.charset = "UTF-8";
+  },
+  
+  get file() this._file,
+  
+  get active() this._active,
+  
   prepareFile: function Logger_prepareFile() {
     if (!this._dir.exists())
       this._dir.create(Ci.nsIFile.DIRECTORY_TYPE, 0777);
     else if (!this._dir.isDirectory())
       throw Error(this._dir.path + " is not a directory.");
-
+  
     var file = this._dir.clone();
-    file.append(new Date().getTime() + ".log");
-
-    var foStream = Cc["@mozilla.org/network/file-output-stream;1"]
-                   .createInstance(Ci.nsIFileOutputStream);
-    foStream.init(file, 0x02 | 0x08 | 0x20, 0666, 0);
-    this.file = file;
+    file.append(Date.now() + ".log");
+    
+    this._file = file;  
   },
-
+  
   start: function Logger_start() {
     if (!this.active) {
       this.prepareFile();
-      this.active = true;
+      this._active = true;
       console.debug("Logging to '" + this.file.path + "' started.");
     }
   },
-
+  
   stop: function Logger_stop() {
     if (this.active) {
-      this.active = false;
+      this._active = false;
       console.debug("Logging to '" + this.file.path + "' stopped.");
     }
   },
-
+  
   log: function Logger_log(data) {
     if (this.active) {
-      var foStream = Cc["@mozilla.org/network/file-output-stream;1"].
-                     createInstance(Ci.nsIFileOutputStream);
-
-      foStream.init(this.file, 0x02 | 0x08 | 0x10, 0666, 0);
-
-      data.timestamp = new Date().getTime();
+      data.timestamp = Date.now();
       var message = JSON.stringify(data);
+      
+      // Create a output stream to write to file
+      var foStream = Cc["@mozilla.org/network/file-output-stream;1"]
+                       .createInstance(Ci.nsIFileOutputStream);
+      foStream.init(this._file, 0x02 | 0x08 | 0x10, 0666, 0);
+      var iStream = this._converter.convertToInputStream(message + '\r\n');
+      
+      // Write asynchronously to buffer;
+      // Input and output streams are closed after write
       console.debug("Logging: '" + message + "'");
-
-      var converter = Cc["@mozilla.org/intl/converter-output-stream;1"].
-                      createInstance(Ci.nsIConverterOutputStream);
-
-      converter.init(foStream, "UTF-8", 0, 0);
-      converter.writeString(message + '\r\n');
-      converter.close();
+      NetUtil.asyncCopy(iStream, foStream, function(status) {
+        if (!Components.isSuccessCode(status)) {
+          errorMessage = new Error("Error while writing to file: " + status);
+          console.error(errorMessage);
+        }
+      });
     }
   }
-}
-
+});  
+  
 exports.Logger = Logger;
--- extension/test/test-logger.js
+++ extension/test/test-logger.js
@@ -1,19 +1,17 @@
 var { Cc, Ci } = require("chrome");
 var { Logger } = require("memchaser/logger")
-
 var dir = Cc["@mozilla.org/file/directory_service;1"]
           .getService(Ci.nsIProperties).get("TmpD", Ci.nsIFile);
 
-
 exports.test_default_to_not_logging = function (test) {
-  var logger = new Logger(dir);
+  var logger = new Logger({aDir: dir});
 
   test.assert(!logger.active);
 }
 
 
 exports.test_start_stop_logging = function (test) {
-  var logger = new Logger(dir);
+  var logger = new Logger({aDir: dir});
 
   logger.start();
   test.assert(logger.active);
@@ -21,5 +19,34 @@ exports.test_start_stop_logging = function (test) {
   logger.stop();
   test.assert(!logger.active);
 
-  logger.file.remove(false);
+  //logger.file.remove(false);
+}
+
+exports.test_log = function (test) {
+  var logger = new Logger({aDir: dir});
+  
+  logger.start();
+  logger.log({GC: 50});
+  logger.log({GC: 60});
+  logger.log({GC: 70});
+
+  
+// open an input stream from file  
+  var istream = Components.classes["@mozilla.org/network/file-input-stream;1"].  
+                createInstance(Components.interfaces.nsIFileInputStream);  
+  istream.init(logger.file, 0x01, 0444, 0);  
+  istream.QueryInterface(Components.interfaces.nsILineInputStream);  
+    
+  // read lines into array  
+  var line = {}, lines = [], hasmore;  
+  do {  
+    hasmore = istream.readLine(line);  
+    lines.push(line.value);   
+  } while(hasmore);  
+  
+  istream.close();  
+  test.assert(lines[0].indexOf(':50') != -1, 'data');
+  test.assert(lines[1].indexOf(':60') != -1, 'data');
+  test.assert(lines[2].indexOf(':70') != -1, 'data');
+  //logger.file.remove(false);  
 }
