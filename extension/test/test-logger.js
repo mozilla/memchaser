eaeccfc85ab388b6fc0b35bb129d435904d618ab
David Guo <david.guo@uwaterloo.ca>
2012-04-03 01:30:17 -0700

    Allows user to specify where logs go (#119)

--- extension/test/test-logger.js
+++ extension/test/test-logger.js
@@ -3,7 +3,41 @@ Components.utils.import('resource://gre/modules/Services.jsm');
 const { Cc, Ci } = require("chrome");
 const { Logger } = require("memchaser/logger")
 
-const dir = Services.dirsvc.get("TmpD", Ci.nsIFile);
+const dir = Services.dirsvc.get("TmpD", Ci.nsILocalFile);
+
+var verifyAsyncOutput = function (test, logger, testFunc) {
+  var message = '';
+
+  return function (status) {
+    var line = {}, hasMore;
+
+    try {
+      // open an input stream from file
+      var istream = Cc["@mozilla.org/network/file-input-stream;1"].
+                    createInstance(Ci.nsIFileInputStream);
+      istream.init(logger.file, 0x01, parseInt("0444",8), 0);
+      istream.QueryInterface(Ci.nsILineInputStream);
+
+      // read lines into array
+      do {
+        hasMore = istream.readLine(line);
+        message += line.value;
+      } while (hasMore);
+    }
+    finally {
+      istream.close();
+      message = JSON.parse(message);
+
+      if (typeof(testFunc) === 'function') {
+        testFunc(message);
+      }
+
+      logger._file.remove(false);
+      test.assert(!logger._file.exists(), "Clean-up; delete test file");
+      test.done();
+    }
+  };
+}
 
 exports.test_default_to_not_logging = function (test) {
   var logger = new Logger({ dir: dir });
@@ -32,43 +66,57 @@ exports.test_start_stop_logging = function (test) {
 
 exports.test_log_and_callback = function (test) {
   var logger = new Logger({ dir: dir });
-  var message = '';
-
-  function verifyOutput(status) {
-    var line = {}, hasMore;
-    try {
-      // open an input stream from file
-      var istream = Cc["@mozilla.org/network/file-input-stream;1"].
-                    createInstance(Ci.nsIFileInputStream);
-      istream.init(logger.file, 0x01, parseInt("0444",8), 0);
-      istream.QueryInterface(Ci.nsILineInputStream);
-
-      // read lines into array
-      do {
-        hasMore = istream.readLine(line);
-        message += line.value;
-      } while (hasMore);
-    }
-    finally {
-      istream.close();
-      message = JSON.parse(message);
-
-      test.assertEqual(message.length, 3);
-      test.assertEqual(message[0].data.x, 50);
-      test.assertEqual(message[1].data.x, 60);
-      test.assertEqual(message[2].data.x, 70);
-
-      logger._file.remove(false);
-      test.assert(!logger._file.exists(), "Clean-up; delete test file");
-      test.done();
-    }
-  }
 
   logger.start();
   logger.log('test', { x: 50 });
   logger.log('test', { x: 60 });
-  logger.log('test', { x: 70 }, verifyOutput);
-  logger.stop();
+  logger.log('test', { x: 70 });
+  logger.stop(verifyAsyncOutput(test, logger, function (message) {
+    test.assertEqual(message.length, 3);
+    test.assertEqual(message[0].data.x, 50);
+    test.assertEqual(message[1].data.x, 60);
+    test.assertEqual(message[2].data.x, 70);
+  }));
+
+  test.waitUntilDone(2000);
+}
+
+exports.test_start_directory_change_nsIFile = function (test) {
+  var logger = new Logger({ dir: dir });
+  var newDir = dir.clone();
+  newDir.append('newdir');
+
+  logger.dir = newDir;
+  logger.start();
+  logger.log('test', { x: 50 });
+  logger.log('test', { x: 60 });
+  logger.log('test', { x: 70 });
+  logger.stop(verifyAsyncOutput(test, logger, function (message) {
+    test.assertEqual(message.length, 3);
+    test.assertEqual(message[0].data.x, 50);
+    test.assertEqual(message[1].data.x, 60);
+    test.assertEqual(message[2].data.x, 70);
+  }));
+    
+  test.waitUntilDone(2000);
+}
+
+exports.test_start_directory_change_string = function (test) {
+  var logger = new Logger({ dir: dir });
+  var newDir = dir.clone();
+  newDir.append('newdir');
+
+  logger.dir = newDir.path;
+  logger.start();
+  logger.log('test', { x: 50 });
+  logger.log('test', { x: 60 });
+  logger.log('test', { x: 70 });
+  logger.stop(verifyAsyncOutput(test, logger, function (message) {
+    test.assertEqual(message.length, 3);
+    test.assertEqual(message[0].data.x, 50);
+    test.assertEqual(message[1].data.x, 60);
+    test.assertEqual(message[2].data.x, 70);
+  }));
 
   test.waitUntilDone(2000);
 }
