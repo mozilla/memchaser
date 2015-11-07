65bf2df8c56460dc863b3522a9f0e6de6ce8402a
Szabolcs Hubai <szab.hu@gmail.com>
2015-11-07 01:04:08 +0100

    Switch from 'cfx' to 'jpm' for packaging (#203)

--- extension/test/test-garbage-collector.js
+++ extension/test/test-garbage-collector.js
@@ -1,9 +1,9 @@
 const prefs = require("sdk/preferences/service");
 
-const config = require("./config");
+const config = require("../lib/config");
 
 exports.test_javascript_memory_pref_enabled = function (test) {
   // We have to require the garbage collector to initialize the module
-  var gc = require("./garbage-collector");
+  var gc = require("../lib/garbage-collector");
   test.assert(prefs.get(gc.reporter.pref_gc_notifications));
 }
